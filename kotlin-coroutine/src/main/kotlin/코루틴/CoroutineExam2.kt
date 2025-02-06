1f698e91e7feac679e1f8bf9c41dd0753730cd85
CodeDDAGGI <qudrb1132@naver.com>
2025-02-06 22:57:07 +0900

    Feat : "추가"

--- kotlin-coroutine/src/main/kotlin/코루틴/CoroutineExam2.kt
+++ kotlin-coroutine/src/main/kotlin/코루틴/CoroutineExam2.kt
@@ -1,10 +1,8 @@
 package com.study.코루틴
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import java.util.concurrent.Executors
+import kotlin.system.measureTimeMillis
 
 //fun downloadImage(url:String):String {
 //    Thread.sleep(3000)
@@ -130,41 +128,67 @@ private val executor = Executors.newFixedThreadPool(3)
 //}
 
 // 개발자님 답
-
-fun saveImg(img : String) {
-    Thread.sleep(1000)
-    println("이미지 데이터: ${img.take(15)}...")
+fun downloadImage(url: String): String {
+    Thread.sleep(3000)
+    return "이미지 데이터: $url"
 }
 
-fun downloadImage(urls: List<String>){
+fun saveToDisk(image: String) {
+    Thread.sleep(2000)
+    println("저장완료: ${image.take(15)}...")
+}
+
+
+fun downloadSync(urls: List<String>) {
+    urls.forEach { url ->
+        val image = downloadImage(url)
+        saveToDisk(image)
+    }
+}
+
+// 비동기
+fun downloadAsync(urls: List<String>) {
     val executor = Executors.newFixedThreadPool(urls.size)
 
-    urls.forEach{
-        url -> executor.submit {
+    urls.forEach { url ->
+        executor.submit {
             val image = downloadImage(url)
-        saveImg(image)
-    }
-    }
-}
-
-fun main() = runBlocking{
-    val imgList = listOf("url1" , "url2" , "url3")
-    val start = System.currentTimeMillis()
-    val reload = imgList.map {
-        img -> launch {
-            val image = downloadImage(img)
-            val down = saveImg(image)
-            println(down)
+            saveToDisk(image)
         }
     }
-
-    reload.forEach{it.join()}
-
-    println("걸리는 시간 : ${System.currentTimeMillis() - start}ms")
 }
 
-suspend fun downloadCoroutine(urls: List<String>) = coroutineScope{
-    urls.map {
-        url ->
+// 코루틴
+suspend fun downloadCoroutine(urls: List<String>) = coroutineScope {
+    urls.map { url ->
+        async(Dispatchers.IO) {
+            val image = downloadImage(url)
+            withContext(Dispatchers.Default) {
+                saveToDisk(image)
+            }
+        }
+    }.awaitAll()
+}
+
+fun main() = runBlocking {
+    // 이미지 다운로드 테스트
+    val urls = listOf("url1", "url2", "url3")
+
+    // 동기 버전
+    val syncTime = measureTimeMillis {
+        downloadSync(urls)
     }
+
+    println("동기 다운로드시간: ${syncTime}ms")
+
+    val threadPoolTime = measureTimeMillis {
+        downloadAsync(urls)
+    }
+    println("스레드 풀 비동기 시간: ${threadPoolTime}ms")
+
+    // 코루틴 버전
+    val asyncTime = measureTimeMillis {
+        downloadCoroutine(urls)
+    }
+    println("코루틴 다운로드 시간: ${asyncTime}ms")
 }
